6ccefc6ab65b32e181be365157299f60d0147186
Aashi Sachdeva <aashisachdeva18@gmail.com>
2020-02-24 00:12:35 +0530

    cpopup

--- inzinbackend/models/categories.js
+++ inzinbackend/models/categories.js
@@ -19,6 +19,11 @@ const categoriesSchema= new Schema({
     content:String,
     keywords:String,
     isParent:Boolean,
+    seo_metatitle:String,
+    seo_metadescription:String,
+    seo_metaheading:String,
+    seo_metaheadingdescription:String,
+    seo_keywords:String,
     
    
   });
